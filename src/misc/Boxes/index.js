d3814f16f4e23452dc09d14a4f605dbb5be019b2
Stephen Cook <stephen.cook@onfido.com>
2018-06-25 22:54:47 +0100

    Final changes

--- src/misc/Boxes/index.js
+++ src/misc/Boxes/index.js
@@ -17,6 +17,7 @@ export const BoxContainer = ({ children }) => (
 export const ABox = () => <Box>A</Box>;
 export const BBox = () => <Box>B</Box>;
 export const CBox = () => <Box>C</Box>;
+export const XBox = ({ x }) => <Box>{x}</Box>;
 export const ABoxCompact = () => <Box compact>A</Box>;
 export const BBoxCompact = () => <Box compact>B</Box>;
 export const CBoxCompact = () => <Box compact>C</Box>;
--- src/slides/04 - why.js
+++ src/slides/04 - why.js
@@ -6,22 +6,25 @@ import { animateSwitch } from "../misc/routerComponents";
 import CustomSlide from "../misc/CustomSlide";
 import SlideOut from "../misc/SlideOut";
 import FadeOut from "../misc/FadeOut";
+import FakeBrowser from "../misc/FakeBrowser";
+import { XBox } from "../misc/Boxes";
 import distracted from "../images/distracted.gif";
 import fancy from "../images/fancy.gif";
 import calm from "../images/calm.mp4";
 import styles from "./common.scss";
 
 const FaderSwitch = animateSwitch(Switch, FadeOut);
+const SliderSwitch = animateSwitch(Switch, SlideOut);
 
 export default () => (
   <CustomSlide>
     <h1>Why?</h1>
 
-    <Step index={0} maxIndex={3}>
+    <Step index={0} maxIndex={10}>
       {""}
     </Step>
     <div className={styles.centerWithHeader}>
-      <FaderSwitch>
+      <FaderSwitch pathKeyGenerator={[{ test: /\/[2-8]$/, pathKey: "#/3/2" }]}>
         <Route
           path="/(.*)/0"
           render={() => (
@@ -37,10 +40,50 @@ export default () => (
 
         <Route path="/(.*)/1" render={() => <img src={distracted} />} />
 
-        <Route path="/(.*)/2" render={() => <img src={fancy} />} />
+        <Route
+          path="/(.*)/([2-5])"
+          render={() => (
+            <FakeBrowser
+              urlMap={{
+                "#/3/2": "https://mysite.com/19582",
+                "#/3/3": "https://mysite.com/65402",
+                "#/3/4": "https://mysite.com/33093",
+                "#/3/5": "https://mysite.com/65204"
+              }}
+            >
+              <Switch>
+                <Route path="/(.*)/2" render={() => <XBox x="19582" />} />
+                <Route path="/(.*)/3" render={() => <XBox x="65402" />} />
+                <Route path="/(.*)/4" render={() => <XBox x="33093" />} />
+                <Route path="/(.*)/5" render={() => <XBox x="65204" />} />
+              </Switch>
+            </FakeBrowser>
+          )}
+        />
 
         <Route
-          path="/(.*)/3"
+          path="/(.*)/([6-8])"
+          render={() => (
+            <FakeBrowser
+              urlMap={{
+                "#/3/6": "https://mysite.com/65402",
+                "#/3/7": "https://mysite.com/65204",
+                "#/3/8": "https://mysite.com/64502"
+              }}
+            >
+              <SliderSwitch order={(a, b) => a.localeCompare(b)}>
+                <Route path="/(.*)/6" render={() => <XBox x="65402" />} />
+                <Route path="/(.*)/7" render={() => <XBox x="65204" />} />
+                <Route path="/(.*)/8" render={() => <XBox x="64502" />} />
+              </SliderSwitch>
+            </FakeBrowser>
+          )}
+        />
+
+        <Route path="/(.*)/9" render={() => <img src={fancy} />} />
+
+        <Route
+          path="/(.*)/10"
           render={() => (
             <video loop={true} autoPlay={true} style={{ maxWidth: "546px" }}>
               <source src={calm} type="video/mp4" />
